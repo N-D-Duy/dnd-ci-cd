7ffe616c702e3b5cb5dcfcd964a6e7fcdfc4d811
duynguyen <nguyenducduypc160903@gmail.com>
2024-08-02 20:45:13 +0700

    fix: github actions workflows

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World dnd-ci-cd!';
+    return 'Hello World!';
   }
 }
